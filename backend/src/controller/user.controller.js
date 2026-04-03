38d64cb37939110255cb3b6b4d47b6a7d80160ed
Your Name <nishantnk476@gmail.com>
2026-04-03 13:58:21 +0530

    learned auth API (login, logout)

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -33,18 +33,54 @@ const registerUser = async (req, res) => {
   }
 };
 const loginUser = async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        const user = User.findOne({
-            email : email.toLowerCase(),
-        });
-        if(!user) {
-            return res.status(400).json({
-                message: "user not found"
-            });
-        }
-    } catch (error) {
-        
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+    if (!user) {
+      return res.status(400).json({
+        message: "user not found",
+      });
     }
-}
-export { registerUser };
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res.status(400).json({
+        message: "invalid cred",
+      });
+    res.status(200).json({
+      message: "logged in",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "internal server error",
+    });
+  }
+};
+
+const logoutUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
+    return res.status(200).json({
+      message: "logged out successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+export { registerUser, loginUser, logoutUser };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controller/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controller/user.controller.js";
 const router = Router();
 
 router.route("/register").post(registerUser); //post is the http method that's why
+router.route("/login").post(loginUser); 
+router.route("/logout").post(logoutUser); //because we can't really delete data,, we defined the method of logiut and some data is created, hence post
 
 export default router;
